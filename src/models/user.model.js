db44b6617a8f8a451b8d9b28eff5bc2c05b601c9
yashPundhir <yash.pundhir.prof@gmail.com>
2025-08-31 23:38:03 +0530

    add user schema hooks

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,5 +1,9 @@
 import mongoose from "mongoose";
 
+import bcrypt from "bcryptjs";
+
+import jwt from "jsonwebtoken";
+
 import { defaultPlaceholderAvatarUrl } from "../constants/constants";
 
 const userSchema = new mongoose.Schema(
@@ -59,6 +63,31 @@ const userSchema = new mongoose.Schema(
   { timestamps: true },
 );
 
-export const User = mongoose.model("User", userSchema);
-
 // Auth Hooks
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateAccessToken = async function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    },
+  );
+};
+
+export const User = mongoose.model("User", userSchema);
