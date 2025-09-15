6897caae0408db82be2d21654a2a50327d97a841
yashPundhir <yash.pundhir.prof@gmail.com>
2025-09-15 23:28:54 +0530

    do the following changes: update todo and notes create health check controller and route create boilerplate router code for other routes

--- src/app.js
+++ src/app.js
@@ -1,5 +1,10 @@
 import express from "express";
 
+// router imports
+import healthCheckRouter from "./routes/healthCheck.routes.js";
+
 const app = express();
+
+app.use("/api/v1/healthCheck", healthCheckRouter);
 
 export default app;
--- src/notes/todo.md
+++ src/notes/todo.md
@@ -1,1 +1,3 @@
 - in mongoose docs, read about `required` for a field
+
+- explore what are `loggers` for a backend system
